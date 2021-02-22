ec233dc9be7e0f4d12949d4341cc3f3c176eafab
SURYA NARAYANA MURTHY <kasyap.surya@gmail.com>
2021-02-22 20:06:15 +0530

    Update test.php

--- test.php
+++ test.php
@@ -22,7 +22,23 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 // Execute the POST request
 $result = curl_exec($ch);
+
+if(!curl_errno($ch))
+{
+    $info = curl_getinfo($ch);
+    if ($info['http_code'] == 200) {
+        echo "Coming to status";
+        print_r($result);
+    }
+}
+else
+{
+  // Error happened
+  $error_message = curl_error($ch);
+  print_r($error_message);
+}
+
 // Close cURL resource
 curl_close($ch);
-
+echo "Final Result";
 print_r($result);
